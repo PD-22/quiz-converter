06a629b81a9245af76fa6de0e71eeedfdca151f4
Pavle <p.devidze@gmail.com>
2022-12-18 15:52:11 +0400

    rename data folder to test

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@ const { parsePdf } = require("./parsePdf");
 const { formatQuiz } = require("./formatQuiz");
 const { genExcel } = require('./genExcel');
 
-start();
+const startPath = join(__dirname, '../test');
 
-async function start() {
-    const inputPath = join(__dirname, '../data/quiz.pdf');
+start(startPath);
+
+async function start(startPath) {
+    const inputPath = join(startPath, '/quiz.pdf');
     const quizText = await parsePdf(inputPath);
 
     const quizObject = formatQuiz(quizText);
     console.log(quizObject);
 
-    const outputPath = join(__dirname, '../data/quiz.xlsx');
+    const outputPath = join(startPath, '/quiz.xlsx');
     const gridInput = [
         ['question1', 'answer1'],
         ['question2', 'answer2'],
