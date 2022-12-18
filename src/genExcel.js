31e449df4da8be40896583c705bb8f6de15dc85c
Pavle <p.devidze@gmail.com>
2022-12-18 11:46:11 +0400

    move files to data folder

--- src/genExcel.js
+++ src/genExcel.js
@@ -1,16 +1,16 @@
 const excel = require('excel4node');
+const { join } = require('path');
 
-genExcel();
+const outputPath = join(__dirname, '../data/quiz.xlsx');
 
 async function genExcel() {
     const workbook = new excel.Workbook();
     const worksheet = workbook.addWorksheet('Sheet 1');
 
     worksheet.cell(1, 1).string('question');
-
     worksheet.cell(1, 2).string('answer');
 
-    workbook.write('quest.xlsx');
+    workbook.write(outputPath);
 }
 
 exports.genExcel = genExcel;
--- src/parsePdf.js
+++ src/parsePdf.js
@@ -1,10 +1,15 @@
 const fs = require('fs/promises');
+const { join } = require('path');
 const pdfParse = require('pdf-parse');
 
+const inputPath = join(__dirname, '../data/quiz.pdf');
+
 async function parsePdf() {
-    const dataBuffer = await fs.readFile('./quest21.pdf');
+    const dataBuffer = await fs.readFile(inputPath);
+
     const parsedPdf = await pdfParse(dataBuffer);
     const text = parsedPdf.text;
+
     console.log(text);
 }
 
