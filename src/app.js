39e614fa4227fd96e82984cabbfddb9ffccb9a31
Pavle <p.devidze@gmail.com>
2022-12-19 23:19:32 +0400

    fix url

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const { genQuizExcel } = require('./genQuizExcel');
 start();
 
 async function start() {
-    const inputPath = join(__dirname, '/test/quiz.pdf');
+    const inputPath = join(__dirname, '../test/quiz.pdf');
     const quizText = await parseQuizPdf(inputPath);
     const quizFormatted = formatQuiz(quizText);
-    const outputPath = join(__dirname, '/test/quiz.xlsx');
+    const outputPath = join(__dirname, '../test/quiz.xlsx');
     await genQuizExcel(outputPath, quizFormatted);
 }
