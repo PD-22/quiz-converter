ca21bb542e3f3d482e49b4de08f89efd3405e6dd
Pavle <p.devidze@gmail.com>
2022-12-25 13:36:19 +0400

    add json quiz output

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const { join } = require('path');
 const { parseQuizPdf } = require("./parseQuizPdf");
 const { formatQuiz } = require("./formatQuiz");
 const { genQuizExcel } = require('./genQuizExcel');
+const { writeFile } = require('fs/promises');
+
+const inputPath = join(__dirname, '../test/quiz.pdf');
+const jsonOutputPath = join(__dirname, '../test/quiz.json');
+const excelOutputPath = join(__dirname, '../test/quiz.xlsx');
 
 start();
 
 async function start() {
-    const inputPath = join(__dirname, '../test/quiz.pdf');
     const quizText = await parseQuizPdf(inputPath);
     const quizFormatted = formatQuiz(quizText);
-    const outputPath = join(__dirname, '../test/quiz.xlsx');
-    await genQuizExcel(outputPath, quizFormatted);
+    await writeFile(jsonOutputPath, JSON.stringify(quizFormatted, null, 2));
+    await genQuizExcel(excelOutputPath, quizFormatted);
 }
